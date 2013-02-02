39a6422d909c7cf3b139088c930ee096d8aaa3bc
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-02-02 13:41:03 +0100

    Finishing the file reader test suite.

--- ses3dpy/tests/test_ses3d_file_parser.py
+++ ses3dpy/tests/test_ses3d_file_parser.py
@@ -133,15 +133,16 @@ class SES3DFileParserTestCase(unittest.TestCase):
         filename_phi = os.path.join(self.data_dir, "File_phi")
         filename_r = os.path.join(self.data_dir, "File_r")
 
-        # The x-component points east.
+        # The theta component goes from north to south but is inverted thus
+        # points north.
         tr_theta = read_SES3D(filename_theta)[0]
         self.assertEqual(tr_theta.stats.channel, "N")
 
-        # The x-component points south, but is inverted, thus north.
+        # The phi component goes from west to east.
         tr_phi = read_SES3D(filename_phi)[0]
         self.assertEqual(tr_phi.stats.channel, "E")
 
-        # The z-component points up.
+        # The r-component points up.
         tr_r = read_SES3D(filename_r)[0]
         self.assertEqual(tr_r.stats.channel, "Z")
 
@@ -161,6 +162,27 @@ class SES3DFileParserTestCase(unittest.TestCase):
         data *= -1.0
         # Check.
         np.testing.assert_almost_equal(tr.data[-10:], data)
+
+    def test_OtherComponentsAreNotInverted(self):
+        """
+        The other components should not be inverted.
+        """
+        filename_phi = os.path.join(self.data_dir, "File_phi")
+        filename_r = os.path.join(self.data_dir, "File_r")
+
+        tr_phi = read_SES3D(filename_phi)[0]
+        self.assertEqual(tr_phi.stats.channel, "E")
+        phi_data = np.array([4.23160685E-07, 3.80973177E-07, 3.39335969E-07,
+            2.98305707E-07, 2.57921158E-07, 2.18206054E-07, 1.79171423E-07,
+            1.40820376E-07, 1.03153077E-07, 6.61708626E-08])
+        np.testing.assert_almost_equal(tr_phi.data[-10:], phi_data)
+
+        tr_r = read_SES3D(filename_r)[0]
+        self.assertEqual(tr_r.stats.channel, "Z")
+        r_data = np.array([3.33445854E-07, 3.32186886E-07, 3.32869206E-07,
+            3.35317537E-07, 3.39320707E-07, 3.44629825E-07, 3.50957549E-07,
+            3.57983453E-07, 3.65361842E-07, 3.72732785E-07])
+        np.testing.assert_almost_equal(tr_r.data[-10:], r_data)
 
 
 def suite():
