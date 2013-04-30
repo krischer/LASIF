8b33fde4b56b6ae25098436629841b3337f0889c
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-04-30 18:42:31 +0200

    Adding time frequency cross correlation difference.

--- lasif/adjoint_sources/time_frequency.py
+++ lasif/adjoint_sources/time_frequency.py
@@ -56,3 +56,56 @@ def time_frequency_transform(t, s, dt_new, width, threshold):
         else:
             tfs[k, :] = 0.0
     return TAU, NU, tfs
+
+
+def time_frequency_cc_difference(t, s1, s2, dt_new, width, threshold):
+    """
+    Straight port of tfa_cc_new.m
+
+    :param t: discrete time
+    :param s1: discrete signal 1
+    :param s2: discrete signal 2
+    :param dt_new: time increment in the tf domain
+    :param width: width of the Gaussian window
+    :param threshold: fraction of the absolute signal below which the Fourier
+        transform is set to zero in order to reduce computation time
+    """
+    # New time axis
+    ti = utils.matlab_range(t[0], t[-1], dt_new)
+    # Interpolate both signals to the new time axis
+    si1 = interp1d(t, s1, kind=1)(ti)
+    si2 = interp1d(t, s2, kind=1)(ti)
+
+    # Extend the time axis, required for the correlation
+    N = len(ti)
+    t_cc = utils.matlab_range(t[0], (2 * N - 2) * dt_new, dt_new)
+
+    # Rename some variables
+    s1 = si1
+    s2 = si2
+    t_min = t_cc[0]
+
+    # Initialize the meshgrid
+    N = len(t_cc)
+    dnu = 1.0 / (N * dt_new)
+
+    nu = utils.smatlab_range(0, float(N - 1) / (N * dt_new), dnu)
+    tau = t_cc
+    TAU, NU = np.meshgrid(tau, nu)
+
+    # Compute the time frequency representation
+    tfs = np.zeros(NU.shape, dtype="complex128")
+
+    for k in xrange(len(tau)):
+        # Window the signals
+        w = utils.gaussian_window(ti - tau[k], width)
+        f1 = w * s1
+        f2 = w * s2
+
+        if np.abs(f1).max() > (threshold * np.abs(s1).max()):
+            cc = utils.cross_correlation(f2, f1)
+            tfs[k, :] = np.fft.fft(cc) / np.sqrt(2.0 * np.pi) * dt_new
+            tfs[k, :] = tfs[k, :] * np.exp(-2.0 * np.pi * 1j * t_min * nu)
+        else:
+            tfs[k, :] = 0.0
+    return TAU, NU, tfs
