Fixed bug in the moment tensor rotation of par2quakeml.

lasif/tools/par2quakeml.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Convert old SES3D Par files to QuakeML
+Convert old SES3D Par files to QuakeML 
 
 **Usage:**
 
-Input filenames of Par file and the desired filename of the output QuakeML
-file, including the full path.
+Still to be specified.
+
 
 
 :copyright:
@@ -15,55 +15,89 @@ file, including the full path.
     GNU General Public License, Version 3
     (http://www.gnu.org/copyleft/gpl.html)
 """
+
+import sys
+sys.path.append('../')
+import lasif.rotations as rot
 import numpy as np
 from obspy.core import UTCDateTime
-from obspy.core.event import Catalog, Magnitude, Event, Origin, \
-    FocalMechanism, Tensor, MomentTensor, NodalPlanes
+from obspy.core.event import Catalog, Magnitude, Event, Origin, FocalMechanism, Tensor, MomentTensor, NodalPlanes
 
-import lasif.rotations as rot
+def par2quakeml(Par_filename,QuakeML_filename,rotation_axis=[0.0,1.0,0.0],rotation_angle=-57.5,origin_time="2000-01-01 00:00:00.0",event_type="other event"):
 
+    # initialise event
+    ev=Event()
 
-def par2quakeml(par_filename, output_filename, rotation_axis=[0.0, 1.0, 0.0],
-        rotation_angle=-57.5, origin_time="2000-01-01 00:00:00.0"):
+    # open and read Par file
+    fid=open(Par_filename,'r')
 
-    # Open file and extract coordinates and moment tensor values
-    with open(par_filename, "rt") as fp:
-        lines = [_i.split()[0] for _i in fp.readlines()]
-    colat, lon, depth = map(float, lines[4:7])
-    lat = rot.colat2lat(colat)
-    mtt, mpp, mrr, mtp, mtr, mpr = map(float, lines[8:14])
+    fid.readline()
+    fid.readline()
+    fid.readline()
+    fid.readline()
 
-    # Rotate into physical domain
-    lat, lon = rot.rotate_lat_lon(lat, lon, rotation_axis, rotation_angle)
-    mrr, mtt, mpp, mtr, mpr, mtp = rot.rotate_moment_tensor(mrr, mtt, mpp, mtr,
-        mpr, mtp, lat, lon, rotation_axis, rotation_angle)
+    lat_old=90.0-float(fid.readline().strip().split()[0])
+    lon_old=float(fid.readline().strip().split()[0])
+    depth=float(fid.readline().strip().split()[0])
 
-    # Initialise event
-    ev = Event(event_type="earthquake")
+    fid.readline()
 
-    ev_origin = Origin(time=UTCDateTime(origin_time), latitude=lat,
-        longitude=lon, depth=depth)
+    Mtt_old=float(fid.readline().strip().split()[0])
+    Mpp_old=float(fid.readline().strip().split()[0])
+    Mrr_old=float(fid.readline().strip().split()[0])
+    Mtp_old=float(fid.readline().strip().split()[0])
+    Mtr_old=float(fid.readline().strip().split()[0])
+    Mpr_old=float(fid.readline().strip().split()[0])
+
+    # rotate event into physical domain
+
+    lat,lon=rot.rotate_lat_lon(lat_old, lon_old, rotation_axis, rotation_angle)
+    Mrr,Mtt,Mpp,Mtr,Mpr,Mtp=rot.rotate_moment_tensor(Mrr_old, Mtt_old, Mpp_old, Mtr_old, Mpr_old, Mtp_old, lat_old, lon_old, rotation_axis, rotation_angle)
+
+    # populate event origin data
+
+    ev.event_type=event_type
+
+    ev_origin=Origin()
+    ev_origin.time=UTCDateTime(origin_time) 
+    ev_origin.latitude=lat
+    ev_origin.longitude=lon
+    ev_origin.depth=depth
     ev.origins.append(ev_origin)
 
     # populte event moment tensor
-    ev_tensor = Tensor(m_rr=mrr, m_tt=mtt, m_pp=mpp, m_rt=mtr, m_rp=mpr,
-        m_tp=mtp)
 
-    ev_momenttensor = MomentTensor(tensor=ev_tensor)
-    ev_momenttensor.scalar_moment = np.sqrt(mrr ** 2 + mtt ** 2 + mpp ** 2 +
-        mtr ** 2 + mpr ** 2 + mtp ** 2)
+    ev_tensor=Tensor()
+    ev_tensor.m_rr=Mrr
+    ev_tensor.m_tt=Mtt
+    ev_tensor.m_pp=Mpp
+    ev_tensor.m_rt=Mtr
+    ev_tensor.m_rp=Mpr
+    ev_tensor.m_tp=Mtp
 
-    ev_focalmechanism = FocalMechanism(moment_tensor=ev_momenttensor)
-    ev_focalmechanism.nodal_planes = NodalPlanes().setdefault(0, 0)
+    ev_momenttensor=MomentTensor()
+    ev_momenttensor.tensor=ev_tensor
+    ev_momenttensor.scalar_moment=np.sqrt(Mrr**2+Mtt**2+Mpp**2+Mtr**2+Mpr**2+Mtp**2)
+
+    ev_focalmechanism=FocalMechanism()
+    ev_focalmechanism.moment_tensor=ev_momenttensor
+    ev_focalmechanism.nodal_planes=NodalPlanes().setdefault(0,0)
 
     ev.focal_mechanisms.append(ev_focalmechanism)
 
     # populate event magnitude
-    ev_magnitude = Magnitude()
-    ev_magnitude.mag = 0.667 * (np.log10(ev_momenttensor.scalar_moment) - 9.1)
-    ev_magnitude.magnitude_type = 'Mw'
+
+    ev_magnitude=Magnitude()
+    ev_magnitude.mag=0.667*(np.log10(ev_momenttensor.scalar_moment)-9.1)
+    ev_magnitude.magnitude_type='Mw'
     ev.magnitudes.append(ev_magnitude)
 
     # write QuakeML file
-    cat = Catalog(events=[ev])
-    cat.write(output_filename, format="quakeml", validate=True)
+
+    cat=Catalog()
+    cat.append(ev)
+    cat.write(QuakeML_filename, format="quakeml")
+ 
+
+    # clean up
+    fid.close()