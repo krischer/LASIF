42e119466e1edf745bced892cd74e9846688bcb8
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-06-10 17:50:30 +0200

    Updating doc.

--- index-5.py
+++ index-5.py
@@ -1,11 +1,5 @@
 import lasif.visualization
-import obspy
-import numpy as np
-def filtered_heaviside(npts, delta, freqmin, freqmax):
-    trace = obspy.Trace(data=np.ones(npts))
-    trace.stats.delta = delta
-    trace.filter("lowpass", freq=freqmax, corners=5)
-    trace.filter("highpass", freq=freqmin, corners=2)
-    return trace.data
-data = filtered_heaviside(1500, 0.75, 1.0 / 500.0, 1.0 / 60.0)
-lasif.visualization.plot_tf(data, 0.75)
+from lasif.source_time_functions import filtered_heaviside
+
+data = filtered_heaviside(4000, 0.13, 1.0 / 500.0, 1.0 / 60.0)
+lasif.visualization.plot_tf(data, 0.13)
