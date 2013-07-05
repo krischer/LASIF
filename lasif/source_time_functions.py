62b2410e6509c9e416f81718987881a90a37f9bc
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-07-05 15:40:17 +0200

    Better documentation for the STF.

--- lasif/source_time_functions.py
+++ lasif/source_time_functions.py
@@ -18,9 +18,17 @@ def filtered_heaviside(npts, delta, freqmin, freqmax):
     Filtered Heaviside source time function.
 
     Freqmin and freqmax specify the bandpass filter frequencies.
+
+    :param npts: The desired number of samples.
+    :param delta: The sample spacing.
+    :param freqmin: The minimum desired frequency.
+    :param freqmax:  The maximum desired frequency.
     """
     trace = obspy.Trace(data=np.ones(npts))
     trace.stats.delta = delta
+    # The number of corners have been empirically determined to yield a nice
+    # source time function. The lowpass is much sharper to ensure that no
+    # energy leaks above the maximum frequency.
     trace.filter("lowpass", freq=freqmax, corners=5)
     trace.filter("highpass", freq=freqmin, corners=2)
 
