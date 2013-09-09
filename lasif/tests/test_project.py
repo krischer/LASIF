137f937ee0e83dec0e89f204e968a7df6bc154ed
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-09-09 11:57:07 +0200

    Added tests for config file reading and caching.

--- lasif/tests/test_project.py
+++ lasif/tests/test_project.py
@@ -9,8 +9,10 @@ Test cases for the project class.
     GNU General Public License, Version 3
     (http://www.gnu.org/copyleft/gpl.html)
 """
+import copy
 import os
 import pytest
+import time
 
 from lasif.project import Project, LASIFException
 
@@ -60,5 +62,79 @@ def test_project_creation(tmpdir):
     assert pr.paths["events"] == os.path.join(tmpdir, "EVENTS")
     assert pr.paths["kernels"] == os.path.join(tmpdir, "KERNELS")
 
-    # Assert the config file.
+    # Assert that the config file has been created.
     assert os.path.exists(os.path.join(pr.paths["root"], "config.xml"))
+
+
+def test_config_file_creation_and_parsing(tmpdir):
+    """
+    Tests the creation of a default config file and the reading of the file.
+    """
+    # Create a new project.
+    pr = Project(str(tmpdir), init_project="TestProject")
+    del pr
+
+    # Init it once again.
+    pr = Project(str(tmpdir))
+
+    # Assert the config file will test the creation of the default file and the
+    # reading.
+    assert pr.config["name"] == "TestProject"
+    assert pr.config["description"] == ""
+    assert pr.config["download_settings"]["arclink_username"] is None
+    assert pr.config["download_settings"]["seconds_before_event"] == 300
+    assert pr.config["download_settings"]["seconds_after_event"] == 3600
+    assert pr.domain["bounds"]["minimum_longitude"] == -20
+    assert pr.domain["bounds"]["maximum_longitude"] == 20
+    assert pr.domain["bounds"]["minimum_latitude"] == -20
+    assert pr.domain["bounds"]["maximum_latitude"] == 20
+    assert pr.domain["bounds"]["minimum_depth_in_km"] == 0.0
+    assert pr.domain["bounds"]["maximum_depth_in_km"] == 200.0
+    assert pr.domain["bounds"]["boundary_width_in_degree"] == 3.0
+    assert pr.domain["rotation_axis"] == [1.0, 1.0, 1.0]
+    assert pr.domain["rotation_angle"] == -45.0
+
+
+def test_config_file_caching(tmpdir):
+    """
+    The config file is cached to read if faster as it is read is every single
+    time a LASIF command is executed.
+    """
+    # Create a new project.
+    pr = Project(str(tmpdir), init_project="TestProject")
+    config = copy.deepcopy(pr.config)
+    domain = copy.deepcopy(pr.domain)
+    del pr
+
+    # Check that the config file cache has been created.
+    cache = os.path.join(str(tmpdir), "CACHE", "config.xml_cache.pickle")
+    assert os.path.exists(cache)
+
+    # Delete it.
+    os.remove(cache)
+    assert not os.path.exists(cache)
+
+    a = time.time()
+    pr = Project(str(tmpdir), init_project="TestProject")
+    b = time.time()
+    time_for_non_cached_read = b - a
+
+    # Assert that everything is still the same.
+    assert config == pr.config
+    assert domain == pr.domain
+    del pr
+
+    # This should have created the cached file.
+    assert os.path.exists(cache)
+
+    a = time.time()
+    pr = Project(str(tmpdir), init_project="TestProject")
+    b = time.time()
+    time_for_cached_read = b - a
+
+    # A cached read should always be faster.
+    assert time_for_cached_read < time_for_non_cached_read
+
+    # Assert that nothing changed.
+    assert config == pr.config
+    assert domain == pr.domain
