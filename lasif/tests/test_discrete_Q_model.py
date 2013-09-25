1116c9feb7d1d4c8e5acb033a1040cb61f832c69
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-09-25 09:51:57 +0200

    Adjusting the test to the algorithm being an optimization

--- lasif/tests/test_discrete_Q_model.py
+++ lasif/tests/test_discrete_Q_model.py
@@ -16,6 +16,10 @@ import numpy as np
 def test_weights_and_relaxation_times():
     """
     Regression test for the weights and relaxation times.
+
+    Due to them being the result of a non-linear and non-unique inversion, the
+    results are different from run to run thus they are hard to test. At least
+    this test somehow asserts that the function does not fail.
     """
     # The last two return values are the frequency dependent Q factor and
     # the phase velocity. Useful for plotting but not here.
@@ -27,7 +31,11 @@ def test_weights_and_relaxation_times():
         max_iterations=10000,
         initial_temperature=0.1,
         cooling_factor=0.9998)
-    np.testing.assert_arrays_almost_equal(
-        weights, [2.51074556, 2.46112185, 0.05143481])
-    np.testing.assert_arrays_almost_equal(
-        relaxation_times, [1.73495054, 14.47838511, 20.62732302])
+    assert len(weights) == 3
+    assert len(relaxation_times) == 3
+    # The big weights are usually similar enough.
+    np.testing.assert_array_almost_equal(
+        sorted(weights)[1:], [2.46112185, 2.51074556], 1)
+    # The relaxation times can differ quite a bit from realization to
+    # realization. Thus they are not really tested.
+    assert sum(relaxation_times) > 10.0
