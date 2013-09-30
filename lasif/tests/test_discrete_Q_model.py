d41beb354360887e9442bf0273779dfe68892848
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-09-30 18:05:33 +0200

    Refactoring. Also replaced Windows linendings

--- lasif/tests/test_discrete_Q_model.py
+++ lasif/tests/test_discrete_Q_model.py
@@ -9,7 +9,6 @@ Simple tests for the constant Q model calculator.
     GNU Lesser General Public License, Version 3
     (http://www.gnu.org/copyleft/lesser.html)
 """
-import random
 import numpy as np
 
 from lasif.tools.Q_discrete import calculate_Q_model
@@ -20,7 +19,7 @@ def test_weights_and_relaxation_times():
     Regression test for the weights and relaxation times.
     """
     # Set the seed to get reproducible results.
-    random.seed(12345)
+    np.random.seed(12345)
 
     # These are the D_p and tau_p, respectively.
     weights, relaxation_times, = calculate_Q_model(
@@ -28,11 +27,11 @@ def test_weights_and_relaxation_times():
         N=3,
         f_min=1.0 / 100.0,
         f_max=1.0 / 10.0,
-        max_iterations=10000,
+        iterations=10000,
         initial_temperature=0.1,
         cooling_factor=0.9998)
 
     np.testing.assert_array_almost_equal(
-        weights, np.array([2.50960201, 2.31899515, 0.19681762]))
+        weights, np.array([2.91588067, 0.92208243, 0.29158807]))
     np.testing.assert_array_almost_equal(
-        relaxation_times, np.array([1.73160984, 14.41562154, 16.70330157]))
+        relaxation_times, np.array([3.70645806, 11.72084951, 37.06458055]))
--- lasif/tools/Q_discrete.py
+++ lasif/tools/Q_discrete.py
@@ -15,10 +15,9 @@ tau is computed from the target Q via 1/Q=0.5*tau*pi
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import random
 
 
-def calculate_Q_model(Q, N, f_min, f_max, max_iterations=10000,
+def calculate_Q_model(Q, N, f_min, f_max, iterations=10000,
                       initial_temperature=0.1, cooling_factor=0.9998):
     """
     :type Q: float
@@ -29,48 +28,47 @@ def calculate_Q_model(Q, N, f_min, f_max, max_iterations=10000,
     :param f_min: Minimum frequency for the discrete-case optimization in Hz.
     :type f_max: float
     :param f_max: Maximum frequency for the discrete-case optimization in Hz.
+    :type iterations: int, optional
+    :param iterations: The number of iterations performed.
+    :type initial_temperature: float, optional
+    :param initial_temperature: The initial temperature for the simulated
+        annealing process.
+    :type cooling_factor: float, optional
+    :param cooling_factor: The cooling factor for the simulated annealing.
     """
-    # Make logarithmic frequency axis
+    # Make a sparse logarithmic frequency axis
     f = np.logspace(np.log10(f_min), np.log10(f_max), 100)
+    # Angular frequency.
     w = 2.0 * np.pi * f
-
-    # compute tau from target Q
-    tau = 2.0 / (np.pi * Q)
 
     # computations for discrete absorption-band model
     # compute initial relaxation times: logarithmically distributed relaxation
     # times
-    tau_min = 1.0 / f_max
-    tau_max = 1.0 / f_min
-    tau_p = np.logspace(np.log10(tau_min), np.log10(tau_max), N) / \
+    tau_p = np.logspace(np.log10(1.0 / f_max), np.log10(1.0 / f_min), N) / \
         (2.0 * np.pi)
 
-    # compute initial weights
+    # compute initial normalized weights
     D_p = 1.0 / tau_p
-    D_p /= sum(D_p)
+    D_p /= D_p.sum()
 
     # compute initial sum that we later force to pi/2 over the frequency range
     # of interest
     S = 0.0
     for n in xrange(N):
         S += D_p[n] * w * tau_p[n] / (1 + w ** 2 * tau_p[n] ** 2)
-
     # compute initial misfit
     chi = sum((S - np.pi / 2.0) ** 2)
 
     # random search for optimal parameters
-    tau_p_test = np.arange(N, dtype=float)
-    D_p_test = np.arange(N, dtype=float)
+    tau_p_test = np.empty(N, dtype=float)
+    D_p_test = np.empty(N, dtype=float)
 
-    for _i in xrange(max_iterations):
+    for _i in xrange(iterations):
         # compute perturbed parameters
-        # For some inexplicable reason this is faster then using
-        # np.random.random(N) and doing it all in one go.
-        for k in xrange(N):
-            tau_p_test[k] = tau_p[k] * (1.0 + (0.5 - random.random()) *
-                                        initial_temperature)
-            D_p_test[k] = D_p[k] * (1.0 + (0.5 - random.random()) *
-                                    initial_temperature)
+        tau_p_test[:] = tau_p * (1.0 + (0.5 - np.random.random()) *
+                                 initial_temperature)
+        D_p_test[:] = D_p * (1.0 + (0.5 - np.random.random()) *
+                             initial_temperature)
 
         # compute new S
         S_test = 0.0
@@ -79,65 +77,78 @@ def calculate_Q_model(Q, N, f_min, f_max, max_iterations=10000,
                 (1 + w ** 2 * tau_p_test[k] ** 2)
 
         # compute new misfit and new temperature
-        chi_test = sum((S_test-np.pi / 2.0) ** 2)
-        initial_temperature = initial_temperature * cooling_factor
+        chi_test = ((S_test - np.pi / 2.0) ** 2).sum()
+        initial_temperature *= cooling_factor
 
-        # check if the tested parameters are better
+        # Check if the tested parameters are better
         if chi_test < chi:
-            D_p[:] = D_p_test[:]
-            tau_p[:] = tau_p_test[:]
+            # Swap references. Faster then copying the values.
+            D_p, D_p_test = D_p_test, D_p
+            tau_p, tau_p_test = tau_p_test, tau_p
             chi = chi_test
+
+    return D_p, tau_p
+
+
+def plot(D_p, tau_p, f_min, f_max, target_Q):
+    """
+    :type D_p: np.ndarray
+    :param D_p: The calculated D_p.
+    :type tau_p: np.ndarray
+    :param tau_p: The calculated tau_p.
+    :type f_min: float
+    :param f_min: Minimum frequency for the discrete-case optimization in Hz.
+        Needed to plot the boundaries.
+    :type f_max: float
+    :param f_max: Maximum frequency for the discrete-case optimization in Hz.
+        Needed to plot the boundaries.
+    :type target_Q: float
+    :param target_Q: The target Q used for the optimization.
+    """
+    # Calculate the plotting boundaries.
+    f_min_plot = 10 ** (np.log10(f_min) - 1)
+    f_max_plot = 10 ** (np.log10(f_max) + 1)
+    f_plot = np.logspace(np.log10(f_min_plot), np.log10(f_max_plot), 100)
 
     # compute optimal Q model
     A = 0.0
     B = 0.0
+    N = len(D_p)
+    # Make a sparse logarithmic frequency axis
+    f = np.logspace(np.log10(f_min), np.log10(f_max), 100)
+    # Angular frequency.
+    w = 2.0 * np.pi * f
+    tau = 2.0 / (np.pi * target_Q)
 
     for n in xrange(N):
         A = A + D_p[n] * w ** 2 * tau_p[n] ** 2 / \
             (1 + w ** 2 * tau_p[n] ** 2)
         B = B + D_p[n] * w * tau_p[n] / (1 + w ** 2 * tau_p[n] ** 2)
 
-    A = 1 + tau*A
+    A = 1 + tau * A
     B = tau * B
 
     Q_discrete = A / B
     v_discrete = np.sqrt(2 * (A ** 2 + B ** 2) /
                          (A + np.sqrt(A ** 2 + B ** 2)))
 
-    return D_p, tau_p, Q_discrete, v_discrete
-
-
-def plot(Q, Q_discrete, v_discrete, f_min, f_max, f_min_plot=1.0/1000.0,
-         f_max_plot=1.0/1.0):
-    """
-    :type f_min_plot: float
-    :param f_min_plot: Minimum plotted frequency in Hz.
-    :type f_max_plot: float
-    :param f_max_plot: Maximum plotted frequency in Hz.
-    """
-    f_plot = np.logspace(np.log10(f_min_plot), np.log10(f_max_plot), 100)
-
-    # minimum frequency [Hz] for plotting
-    f_min_plot = 1.0/1000.0
-    # maximum frequency in [Hz] for plotting
-    f_max_plot = 1.0/1.0
-
     # plot Q and phase velocity as function of frequency
     plt.subplot(121)
-    plt.semilogx([f_min, f_min], [0.9/Q, 1.1/Q], 'r')
-    plt.semilogx([f_max, f_max], [0.9/Q, 1.1/Q], 'r')
-    plt.semilogx([f_min_plot, f_max_plot], [1.0/Q, 1.0/Q], 'b')
-    plt.semilogx(f_plot, 1/Q_discrete, 'k', linewidth=2)
-    plt.xlabel('frequency [Hz]')
-    plt.ylabel('1/Q')
-    plt.title('absorption (1/Q)')
+    plt.semilogx([f_min, f_min], [0.9 / target_Q, 1.1 / target_Q], "r")
+    plt.semilogx([f_max, f_max], [0.9 / target_Q, 1.1 / target_Q], "r")
+    plt.semilogx([f_min_plot, f_max_plot], [1.0 / target_Q, 1.0 / target_Q],
+                 "b")
+    plt.semilogx(f_plot, 1.0 / Q_discrete, "k", linewidth=2)
+    plt.xlabel("frequency [Hz]")
+    plt.ylabel("1/Q")
+    plt.title("absorption (1/Q)")
 
     plt.subplot(122)
-    plt.semilogx([f_min, f_min], [0.9, 1.1], 'r')
-    plt.semilogx([f_max, f_max], [0.9, 1.1], 'r')
-    plt.semilogx(f_plot, v_discrete, 'k', linewidth=2)
-    plt.xlabel('frequency [Hz]')
-    plt.ylabel('v')
-    plt.title('phase velocity')
+    plt.semilogx([f_min, f_min], [0.9, 1.1], "r")
+    plt.semilogx([f_max, f_max], [0.9, 1.1], "r")
+    plt.semilogx(f_plot, v_discrete, "k", linewidth=2)
+    plt.xlabel("frequency [Hz]")
+    plt.ylabel("v")
+    plt.title("phase velocity")
 
     plt.show()
