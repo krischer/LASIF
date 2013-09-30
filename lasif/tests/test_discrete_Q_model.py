bda734a8d3f78e45a98e457f5d84274fa5a82770
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-09-30 16:46:25 +0200

    Using a seed to get reproducible results.

--- lasif/tests/test_discrete_Q_model.py
+++ lasif/tests/test_discrete_Q_model.py
@@ -9,20 +9,21 @@ Simple tests for the constant Q model calculator.
     GNU Lesser General Public License, Version 3
     (http://www.gnu.org/copyleft/lesser.html)
 """
+import random
+import numpy as np
+
 from lasif.tools.Q_discrete import calculate_Q_model
 
 
 def test_weights_and_relaxation_times():
     """
     Regression test for the weights and relaxation times.
-
-    Due to them being the result of a non-linear and non-unique inversion, the
-    results are different from run to run thus they are hard to test. At least
-    this test somehow asserts that the function does not fail.
     """
-    # The last two return values are the frequency dependent Q factor and
-    # the phase velocity. Useful for plotting but not here.
-    weights, relaxation_times, _, _ = calculate_Q_model(
+    # Set the seed to get reproducible results.
+    random.seed(12345)
+
+    # These are the D_p and tau_p, respectively.
+    weights, relaxation_times, = calculate_Q_model(
         Q=100.0,
         N=3,
         f_min=1.0 / 100.0,
@@ -30,13 +31,8 @@ def test_weights_and_relaxation_times():
         max_iterations=10000,
         initial_temperature=0.1,
         cooling_factor=0.9998)
-    assert len(weights) == 3
-    assert len(relaxation_times) == 3
-    # The big weights are usually similar enough.
-    s_weights = sorted(weights)[1:]
-    ref = [2.46112185, 2.51074556]
-    assert abs(s_weights[0] - ref[0]) < 1.0
-    assert abs(s_weights[1] - ref[1]) < 1.0
-    # The relaxation times can differ quite a bit from realization to
-    # realization. Thus they are not really tested.
-    assert sum(relaxation_times) > 10.0
+
+    np.testing.assert_array_almost_equal(
+        weights, np.array([2.50960201, 2.31899515, 0.19681762]))
+    np.testing.assert_array_almost_equal(
+        relaxation_times, np.array([1.73160984, 14.41562154, 16.70330157]))
