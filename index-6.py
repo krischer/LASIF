f71738442e67ccc7044403eb2dc2b880af07fc05
Travis <travis@travis-ci.org>
2013-10-07 14:45:54 +0000

    Travis build 58 pushed to gh-pages

---
+++ index-6.py
@@ -0,0 +1,5 @@
+from lasif.tools import Q_discrete
+weights = [2.56184936462, 2.50613220548, 0.0648624201145]
+relaxation_times = [1.50088990947, 13.3322250004, 22.5140030575]
+Q_discrete.plot(weights, relaxation_times, f_min=1.0 / 100.0,
+                f_max=1.0 / 8.0, show_plot=True)
