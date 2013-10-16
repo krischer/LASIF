ecf363bff940ec4faea8965979ab0b3b5baf21fd
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-10-16 14:06:57 +0200

    Finally succeeded in forcing the mpl backend for the tests

--- conftest.py
+++ conftest.py
@@ -1,2 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This file has a single purpose: Force the use of the "Agg" matplotlib backend
+for the tests. Otherwise lots of windows pop up.
+
+It will be run by pytest, as specified in the pytest.ini file.
+"""
 import matplotlib
 matplotlib.use("agg")
+import matplotlib.pyplot as plt
+plt.switch_backend("agg")
+
+
+def pytest_runtest_setup(item):
+    """
+    This hook is called before every test.
+    """
+    plt.switch_backend("agg")
+    assert matplotlib.get_backend().lower() == "agg"
