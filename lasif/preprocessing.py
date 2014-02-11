bc8cafe8597b57a7c173cc125feba4cf0891c859
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-02-11 20:05:56 +0100

    Converting preprocessing to the new parallel helpers.

--- lasif/preprocessing.py
+++ lasif/preprocessing.py
@@ -9,89 +9,13 @@ Functionality for data preprocessing.
     GNU Lesser General Public License, Version 3
     (http://www.gnu.org/copyleft/lesser.html)
 """
-import numpy as np
-import obspy
-from obspy.xseed import Parser
-import os
-import platform
-import Queue
-from scipy.interpolate import interp1d
-from scipy import signal
 import sys
 import time
-import traceback
 import warnings
 
 from lasif.project import LASIFException
 from lasif.tools.colored_logger import ColoredLogger
-
-
-# The veclib/accelerate framework on OSX is not compatible with
-# multiprocessing. Thus only one core will be used on those. The only
-# proper solution to this is to install numpy/scipy with OpenBLAS and not
-# the Accelerate framework.
-multiproc = True
-if platform.system().lower() == "darwin":
-    config_info = str([value for key, value in
-                       np.__config__.__dict__.iteritems()
-                       if key.endswith("_info")]).lower()
-    if "accelerate" in config_info or "veclib" in config_info:
-        msg = ("The numpy installation on this computer is linked "
-               "against the Accelerate/veclib framework of OSX. "
-               "This is incompatible with multiprocessing and it "
-               "appears that there are no intentions to fix it. "
-               "For this reason only one process will be used for the "
-               "processing. The only proper fix is to link numpy "
-               "against OpenBLAS. Google helps.")
-        warnings.warn(msg)
-        multiproc = False
-
-if multiproc:
-    import multiprocessing
-    parallel_class = multiprocessing.Process
-    queue_class = multiprocessing.Queue
-    process_count = multiprocessing.cpu_count()
-    # Lock used to synchronize I/O.
-    lock = multiprocessing.Lock()
-else:
-    import threading
-    parallel_class = threading.Thread
-    queue_class = Queue.Queue
-    process_count = 1
-    # Lock used to synchronize I/O.
-    lock = threading.Lock()
-
-
-def zerophase_chebychev_lowpass_filter(trace, freqmax):
-    """
-    Custom Chebychev type two zerophase lowpass filter useful for decimation
-    filtering.
-
-    This filter is stable up to a reduction in frequency with a factor of 10.
-    If more reduction is desired, simply decimate in steps.
-
-    Partly based on a filter in ObsPy.
-
-    :param trace: The trace to be filtered.
-    :param freqmax: The desired lowpass frequency.
-
-    Will be replaced once ObsPy has a proper decimation filter.
-    """
-    # rp - maximum ripple of passband, rs - attenuation of stopband
-    rp, rs, order = 1, 96, 1e99
-    ws = freqmax / (trace.stats.sampling_rate * 0.5)  # stop band frequency
-    wp = ws  # pass band frequency
-
-    while True:
-        if order <= 12:
-            break
-        wp *= 0.99
-        order, wn = signal.cheb2ord(wp, ws, rp, rs, analog=0)
-
-    b, a = signal.cheby2(order, rs, wn, btype="low", analog=0, output="ba")
-
-    # Apply twice to get rid of the phase distortion.
-    trace.data = signal.filtfilt(b, a, trace.data)
+from lasif.tools.parallel import parallel_map
 
 
 def preprocess_file(file_info):
@@ -122,7 +46,42 @@ def preprocess_file(file_info):
         * lowpass: The lowpass frequency of the source time function for the
             synthetics.
     """
-    warning_messages = []
+    import numpy as np
+    import obspy
+    from obspy.xseed import Parser
+    from scipy import signal
+    from scipy.interpolate import interp1d
+
+    def zerophase_chebychev_lowpass_filter(trace, freqmax):
+        """
+        Custom Chebychev type two zerophase lowpass filter useful for decimation
+        filtering.
+
+        This filter is stable up to a reduction in frequency with a factor of
+        10. If more reduction is desired, simply decimate in steps.
+
+        Partly based on a filter in ObsPy.
+
+        :param trace: The trace to be filtered.
+        :param freqmax: The desired lowpass frequency.
+
+        Will be replaced once ObsPy has a proper decimation filter.
+        """
+        # rp - maximum ripple of passband, rs - attenuation of stopband
+        rp, rs, order = 1, 96, 1e99
+        ws = freqmax / (trace.stats.sampling_rate * 0.5)  # stop band frequency
+        wp = ws  # pass band frequency
+
+        while True:
+            if order <= 12:
+                break
+            wp *= 0.99
+            order, wn = signal.cheb2ord(wp, ws, rp, rs, analog=0)
+
+        b, a = signal.cheby2(order, rs, wn, btype="low", analog=0, output="ba")
+
+        # Apply twice to get rid of the phase distortion.
+        trace.data = signal.filtfilt(b, a, trace.data)
 
     #==========================================================================
     # Read seismograms and gather basic information.
@@ -131,12 +90,12 @@ def preprocess_file(file_info):
     endtime = starttime + file_info["dt"] * (file_info["npts"] - 1)
     duration = endtime - starttime
 
-    with lock:
-        st = obspy.read(file_info["data_path"])
+    st = obspy.read(file_info["data_path"])
 
     if len(st) != 1:
-        warning_messages.append("The file has %i traces and not 1. "
-                                "Skip all but the first" % len(st))
+        warnings.warn("The file has %i traces and not 1. "
+                      "Skip all but the first" % (
+                      len(st), file_info["data_path"]))
     tr = st[0]
 
     # Trim with a short buffer in an attempt to avoid boundary effects.
@@ -260,170 +219,12 @@ def preprocess_file(file_info):
     #==========================================================================
     # Save processed data and clean up.
     #==========================================================================
-
     # Convert to single precision for saving.
     tr.data = np.require(tr.data, dtype="float32", requirements="C")
     if hasattr(tr.stats, "mseed"):
         tr.stats.mseed.encoding = "FLOAT32"
 
-    with lock:
-        tr.write(file_info["processed_data_path"], format=tr.stats._format)
-
-    return {"warnings": warning_messages}
-
-
-def worker(input_queue, output_queue):
-    """
-    Queue for each worker.
-
-    :param output_queue: The queue where the jobs are stored.
-    :param input_queue: The quere where the results are stored.
-    """
-    # Use "STOP" as the poison pill to stop the worker.
-    for func, args in iter(input_queue.get, "STOP"):
-
-        result = {
-            "exception": None,
-            "filename": args["data_path"],
-            "warnings": []
-        }
-
-        # Catch warnings to be able to log them.
-        with warnings.catch_warnings(record=True) as w:
-            # Function level try/except.
-            try:
-                res = func(args)
-            except:
-                exc_type, exc_class, tb = sys.exc_info()
-                # Make sure it can be pickled to be passed between processes.
-                tb = traceback.extract_tb(tb)
-                result["exception"] = (exc_type, exc_class, tb)
-            else:
-                result["warnings"] = res["warnings"]
-
-            if len(w):
-                for warning in w:
-                    # Catch one specific (benign) warning. This will usually
-                    # come up when designing rather sharp IIR filters for low
-                    # frequencies. The results are still ok. If it fails, it
-                    # would be immediatly noticed.
-                    if "badly conditioned filter coefficients (numerator" in \
-                            str(warning.message).lower():
-                        continue
-                    result["warnings"].append(
-                        "%s: %s" % (str(warning.category), warning.message))
-
-        output_queue.put(result)
-
-
-def pool_imap_unordered(function, iterable):
-    """
-    Custom unordered map implementation. The advantage of this is that it, in
-    contrast to the default Pool.map/imap implementations, does not consume the
-    whole iterable upfront but only when it needs them. This enables infinitely
-    long input queues. This might actually become relevant for LASIF if enough
-    data is present.
-
-    :param function: The function to run for each item.
-    :param iterable: The iterable yielding items.
-    :param processes: The number of processes to launch.
-    """
-    input_queue = queue_class()
-    output_queue = queue_class()
-
-    # Start the worker processes.
-    collected_workers = []
-    for _ in xrange(process_count):
-        collected_workers.append(
-            parallel_class(target=worker, args=(input_queue, output_queue)))
-
-    # Start all processes.
-    for proc in collected_workers:
-        proc.start()
-
-    # Count all sent and received tasks.
-    send_len = 0
-    recv_len = 0
-
-    while True:
-        # Artificial delay to give some times for the queues to play catch-up.
-        time.sleep(0.1)
-
-        # First try to get as many ready results as possible.
-        try:
-            result = output_queue.get_nowait()
-            recv_len += 1
-        # If that fails create new jobs.
-        except Queue.Empty:
-            # Get new values until there are no left. Then break.
-            try:
-                value = iterable.next()
-            except StopIteration:
-                break
-            while True:
-                try:
-                    # Create a new job with the value.
-                    input_queue.put((function, value), timeout=25)
-                    send_len += 1
-                except Queue.Full:
-                    try:
-                        result = output_queue.get(timeout=10)
-                        recv_len += 1
-                    except Queue.Empty:
-                        continue
-                    else:
-                        yield result
-                else:
-                    break
-
-        else:
-            yield result
-
-    # Collect all remaining results. Use a long timeout to catch the final
-    # files.
-    while recv_len < send_len:
-        # Artificial delay to give some times for the queues to play
-        # catch-up.
-        time.sleep(0.1)
-        try:
-            result = output_queue.get(True, 25.0)
-            sys.stdout.flush()
-        except Queue.Empty:
-            result = {
-                "exception": None,
-                "filename": "Unknown",
-                "warnings": []
-            }
-            # Create an exception that can be passed to the parent process.
-            try:
-                msg = "Processing did not finish (deadlock?)"
-                raise LASIFException(msg)
-            except:
-                exc_type, exc_class, tb = sys.exc_info()
-                # Make sure it can be pickled to be passed between processes.
-                tb = traceback.extract_tb(tb)
-                result["exception"] = (exc_type, exc_class, tb)
-        finally:
-            recv_len += 1
-        yield result
-
-    # Terminate the worker processes by passing the poison pill.
-    for _ in xrange(process_count):
-        input_queue.put("STOP")
-
-    time.sleep(0.1)
-
-    for proc in collected_workers:
-        proc.join()
-
-    # Force a shutdown if one or more processes got hung up somewhere.
-    for proc in collected_workers:
-        if proc.is_alive():
-            try:
-                proc.terminate()
-            except:
-                pass
-            proc.join()
+    tr.write(file_info["processed_data_path"], format=tr.stats._format)
 
 
 def launch_processing(data_generator, log_filename=None, waiting_time=4.0,
@@ -441,9 +242,8 @@ def launch_processing(data_generator, log_filename=None, waiting_time=4.0,
     """
     logger = ColoredLogger(log_filename=log_filename)
 
-    logger.info("Launching preprocessing using %i processes...\n"
-                "This might take a while. Press Ctrl + C to cancel.\n"
-                % process_count)
+    logger.info("Launching preprocessing using all processes...\n"
+                "This might take a while. Press Ctrl + C to cancel.\n")
 
     if process_params:
         import pprint
@@ -452,30 +252,20 @@ def launch_processing(data_generator, log_filename=None, waiting_time=4.0,
     # Give the user some time to read the message.
     time.sleep(waiting_time)
 
+    results = parallel_map(preprocess_file,
+                           ({"file_info": i} for i in data_generator))
+
     # Keep track of all files.
     successful_file_count = 0
     warning_file_count = 0
     failed_file_count = 0
-    total_file_count = 0
+    total_file_count = len(results)
 
-    # Result is a dictionary with 3 keys:
-    #   * filename: The name of the current file.
-    #   * exception: None or an actual exception as returned by
-    #                sys.exc_info()
-    #   * warnings: Warnings as a list of strings.
-    for i, result in enumerate(pool_imap_unordered(
-            function=preprocess_file, iterable=data_generator)):
-        warnings.simplefilter("always")
-        total_file_count += 1
-        msg = "Processed file %i: '%s'" % (
-            i, os.path.relpath(result["filename"]))
-        logger.info(msg)
-        if result["exception"] is not None:
-            logger.exception(result["exception"][1])
-            failed_file_count += 1
-        elif result["warnings"]:
-            for warning in result["warnings"]:
-                logger.warning(warning)
+    for result in results:
+        if result.exception is not None:
+            total_file_count += 1
+            continue
+        elif result.warnings:
             warning_file_count += 1
         else:
             successful_file_count += 1
