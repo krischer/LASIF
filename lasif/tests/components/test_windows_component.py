c7e26fae5ad6477a54f08ed61ce184ef5cbfe5bc
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2014-07-08 08:27:58 +0200

    Added test for the windows component.

---
+++ lasif/tests/components/test_windows_component.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import os
+import pytest
+
+from lasif import LASIFNotFoundError
+from lasif.window_manager import MisfitWindowManager
+from ..testing_helpers import communicator  # NOQA
+
+
+@pytest.fixture
+def comm(communicator):
+    communicator.iterations.create_new_iteration(
+        iteration_name=1,
+        solver_name="SES3D_4_1",
+        events_dict=communicator.query.get_stations_for_all_events(),
+        min_period=40.0,
+        max_period=100.0)
+
+    # Create folders for each event.
+    for event_name in communicator.events.list():
+        os.makedirs(os.path.join(communicator.project.paths["windows"],
+                                 event_name))
+
+    # Also create an iteration for one of the events.
+    first_event = sorted(communicator.events.list())[0]
+    os.makedirs(os.path.join(
+        communicator.project.paths["windows"], first_event,
+        communicator.iterations.get_long_iteration_name("1")))
+
+    return communicator
+
+
+def test_window_list(comm):
+    """
+    Tests the list() method.
+    """
+    assert comm.windows.list() == sorted([
+        'GCMT_event_TURKEY_Mag_5.1_2010-3-24-14-11',
+        'GCMT_event_TURKEY_Mag_5.9_2011-5-19-20-15'])
+
+
+def test_window_list_for_event(comm):
+    """
+    Tests the list_for_event() method.
+    """
+    assert comm.windows.list_for_event(
+        'GCMT_event_TURKEY_Mag_5.1_2010-3-24-14-11') == ["1"]
+    assert comm.windows.list_for_event(
+        'GCMT_event_TURKEY_Mag_5.9_2011-5-19-20-15') == []
+
+
+def test_get(comm):
+    """
+    Tests the get() method.
+    """
+    # No known event.
+    with pytest.raises(LASIFNotFoundError) as error:
+        comm.windows.get("RANDOM_EVENT", "RANDOM_ITERATION")
+    assert "event" in str(error.value).lower()
+    assert "not known" in str(error.value).lower()
+
+    # Known event, but unknown iteration.
+    with pytest.raises(LASIFNotFoundError) as error:
+        comm.windows.get('GCMT_event_TURKEY_Mag_5.1_2010-3-24-14-11',
+                         'RANDOM_ITERATION')
+    assert "iteration" in str(error.value).lower()
+    assert "not known" in str(error.value).lower()
+
+    wm = comm.windows.get('GCMT_event_TURKEY_Mag_5.1_2010-3-24-14-11', '1')
+    assert isinstance(wm, MisfitWindowManager)
