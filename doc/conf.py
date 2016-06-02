3ec32c3bee2d9fdada7370b7c65363db99b6ac82
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-06-02 16:47:14 +0200

    Fixing doc building

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, os.path.abspath("."))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
-              'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax',
+              'sphinx.ext.todo', 'sphinx.ext.mathjax',
               'sphinx.ext.viewcode', 'matplotlib.sphinxext.plot_directive',
               'lasif_cli_sphinx_ext', 'autorun']
 
