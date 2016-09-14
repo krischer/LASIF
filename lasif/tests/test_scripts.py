9d730aa2ea3f4320fc6c53691b61d713ad35e896
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-09-14 13:29:49 +0200

    Fixing test for the ses3d setup helper

--- lasif/tests/test_scripts.py
+++ lasif/tests/test_scripts.py
@@ -40,7 +40,7 @@ def test_ses3d_helper_script(capfd):
         "Wave velocities range from 3.9 km/s to 11.7 km/s. The velocities of"
         " the top 15 km have not been analyzed to avoid very slow layers.",
         "Maximal recommended time step: 0.071 s",
-        "Minimal resolvable period: 14.7 s",
+        "Minimal resolvable period: 11.8 s",
         "SES3D Settings: nx_global: 108, ny_global: 168, nz_global: 64",
         "px: 12, py: 14, px: 8",
         "",
@@ -55,7 +55,7 @@ def test_ses3d_helper_script(capfd):
         "Wave velocities range from 3.9 km/s to 11.7 km/s. The velocities "
         "of the top 15 km have not been analyzed to avoid very slow layers.",
         "Maximal recommended time step: 0.071 s",
-        "Minimal resolvable period: 14.3 s",
+        "Minimal resolvable period: 11.4 s",
         "SES3D Settings: nx_global: 108, ny_global: 168, nz_global: 64",
         "px: 12, py: 21, px: 8",
         "",
@@ -70,7 +70,7 @@ def test_ses3d_helper_script(capfd):
         "Wave velocities range from 3.9 km/s to 11.7 km/s. The velocities "
         "of the top 15 km have not been analyzed to avoid very slow layers.",
         "Maximal recommended time step: 0.071 s",
-        "Minimal resolvable period: 14.3 s",
+        "Minimal resolvable period: 11.4 s",
         "SES3D Settings: nx_global: 108, ny_global: 168, nz_global: 64",
         "px: 12, py: 24, px: 8"]
 
